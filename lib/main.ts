494de7924d3a9cd33cbc5e259e05adf50d2c6848
git710 <wl950702@gmail.com>
2024-07-10 23:41:29 +0800

    feat: add 函数改为拼接字符串

--- lib/main.ts
+++ lib/main.ts
@@ -1,11 +1,11 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0;
+  let counter = 0
   const setCounter = (count: number) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
-  };
-  element.addEventListener("click", () => setCounter(++counter));
-  setCounter(0);
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(++counter))
+  setCounter(0)
 }
 
 export function add(a: number, b: number): number {
