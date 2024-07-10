8343e120e08c613776eb172b44169a5b146fb84a
wong <wl950702@gmail.com>
2024-07-10 23:35:54 +0800

    feat: 添加 add 函数

--- lib/main.ts
+++ lib/main.ts
@@ -7,3 +7,7 @@ export function setupCounter(element: HTMLButtonElement) {
   element.addEventListener("click", () => setCounter(++counter));
   setCounter(0);
 }
+
+export function add(a: number, b: number) {
+  return a + b
+}
