baaa5d0f22f9d17a7c3605c39c4627f30ef3b5cd
git710 <wl950702@gmail.com>
2024-07-14 14:34:19 +0800

    feat: 添加减法函数方法

--- lib/main.ts
+++ lib/main.ts
@@ -1,13 +1,17 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(++counter))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(++counter));
+  setCounter(0);
 }
 
 export function add(a: number, b: number): number {
-  return a + b
+  return a + b;
+}
+
+export function substract(a: number, b: number): number {
+  return a - b;
 }
